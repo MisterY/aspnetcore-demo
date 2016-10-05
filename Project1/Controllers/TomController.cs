1dc4c427d429516247dc8d77095d6b20443093f4
Alen Siljak <alen.siljak@yahoo.com>
2016-10-05 14:14:37 +0200

    Displaying a list of toms, link to details page

---
+++ Project1/Controllers/TomController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using DataModel;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WebApp.Controllers
+{
+    public class TomController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            var model = new MyContext().Tom.ToList();
+            return View(model);
+        }
+
+        public IActionResult Detail(string id)
+        {
+            var model = new MyContext().Tom.FirstOrDefault(x => x.Name == id);
+
+            return View(model);
+        }
+    }
+}
