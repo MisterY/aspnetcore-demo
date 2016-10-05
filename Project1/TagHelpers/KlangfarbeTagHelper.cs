fe75501f57ce78dec3b25390cab0ab28660b57e9
Alen Siljak <alen.siljak@yahoo.com>
2016-10-05 13:02:31 +0200

    adding a view component

---
+++ Project1/TagHelpers/KlangfarbeTagHelper.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Project1.TagHelpers
+{
+    [HtmlTargetElement("img", Attributes = "my-url")]
+    public class KlangfarbeTagHelper : TagHelper
+    {
+        [HtmlAttributeName("my-url")]
+        public string Url { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            //base.Process(context, output);
+            //output.Attributes["src"]. = ;
+            //output.Attributes.Clear();
+            output.Attributes.Add("src", "https://www.klangfarbe.com/bilder/A/A-A41554-1-2.jpg");
+        }
+    }
+}
---
+++ Project1/ViewComponents/AuthenticationViewComponent.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Project1.ViewComponents
+{
+    public class AuthenticationViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            var user = User.Identity.Name;
+
+            return View(user);
+        }
+    }
+}
