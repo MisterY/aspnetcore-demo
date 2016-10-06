7928703ccc0e115d18c426baa43e2094be7af298
Alen Siljak <alen.siljak@yahoo.com>
2016-10-06 13:37:00 +0200

    adding sqlite support for entity framework

---
+++ MvcApp/Data/DrumsContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace MvcApp.Data
+{
+    public class DrumsContext : DbContext
+    {
+        public DrumsContext() : base("name=DrumsConnection")
+        {
+
+        }
+
+        public virtual DbSet<Drumset> Drumsets { get; set; }
+    }
+}
---
+++ MvcApp/Data/Drumset.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MvcApp.Data
+{
+    public class Drumset
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Manufacturer { get; set; }
+    }
+}
